85de73516ba3f4ed37eeb1b2f6ee9b293c2b8c80
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-07-03 00:15:49 -0300

    chore: new dependencies and config rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,13 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   plugins: ['import'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {extensions: ['.js', '.jsx', '.ts', '.tsx']},
+    ],
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
