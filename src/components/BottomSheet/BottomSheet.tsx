533794bf09904a82a10b163e0834aa91a606d4d8
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-07-10 12:53:47 -0300

    feat: create bottom sheet component

---
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -0,0 +1,141 @@
+import React, {useCallback, useImperativeHandle} from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedProps,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+
+import {Box} from '../Box/Box';
+
+const {height: SCREEN_HEIGHT} = Dimensions.get('window');
+
+const MAX_TRANSLATE_Y = -SCREEN_HEIGHT + 50;
+
+type BottomSheetProps = {
+  children?: React.ReactNode;
+};
+
+export type BottomSheetRefProps = {
+  scrollTo: (destination: number) => void;
+  isActive: () => boolean;
+};
+
+export const BottomSheet = React.forwardRef<
+  BottomSheetRefProps,
+  BottomSheetProps
+>(({children}, ref) => {
+  const translateY = useSharedValue(0);
+  const active = useSharedValue(false);
+
+  const scrollTo = useCallback(
+    (destination: number) => {
+      'worklet';
+      active.value = destination !== 0;
+
+      translateY.value = withSpring(destination, {damping: 50});
+    },
+    [active, translateY],
+  );
+
+  const isActive = useCallback(() => {
+    return active.value;
+  }, [active.value]);
+
+  useImperativeHandle(ref, () => ({scrollTo, isActive}), [scrollTo, isActive]);
+
+  const context = useSharedValue({y: 0});
+  const gesture = Gesture.Pan()
+    .onStart(() => {
+      context.value = {y: translateY.value};
+    })
+    .onUpdate(event => {
+      translateY.value = event.translationY + context.value.y;
+      translateY.value = Math.max(translateY.value, MAX_TRANSLATE_Y);
+    })
+    .onEnd(() => {
+      if (translateY.value > -SCREEN_HEIGHT / 3) {
+        scrollTo(0);
+      } else if (translateY.value < -SCREEN_HEIGHT / 1.5) {
+        scrollTo(MAX_TRANSLATE_Y);
+      }
+    });
+
+  const rBottomSheetStyle = useAnimatedStyle(() => {
+    const borderRadius = interpolate(
+      translateY.value,
+      [MAX_TRANSLATE_Y + 50, MAX_TRANSLATE_Y],
+      [25, 5],
+      Extrapolate.CLAMP,
+    );
+
+    return {
+      borderRadius,
+      transform: [{translateY: translateY.value}],
+    };
+  });
+
+  const rBackdropStyle = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(active.value ? 1 : 0),
+    };
+  }, []);
+
+  const rBackdropProps = useAnimatedProps(() => {
+    return {
+      pointerEvents: active.value ? 'auto' : 'none',
+    } as any;
+  }, []);
+
+  return (
+    <>
+      <Animated.View
+        onTouchStart={() => {
+          scrollTo(0);
+        }}
+        animatedProps={rBackdropProps}
+        style={[
+          {
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: 'rgba(0,0,0,0.4)',
+          },
+          rBackdropStyle,
+        ]}
+      />
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={[$styles.animatedView, rBottomSheetStyle]}>
+          <Box
+            width={75}
+            height={4}
+            backgroundColor="gray4"
+            mt="s4"
+            mb="s12"
+            borderRadius="s8"
+            alignSelf="center"
+          />
+          {children}
+        </Animated.View>
+      </GestureDetector>
+    </>
+  );
+});
+
+const $styles = StyleSheet.create({
+  animatedView: {
+    height: SCREEN_HEIGHT,
+    width: '100%',
+    backgroundColor: 'white',
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    borderRadius: 24,
+    position: 'absolute',
+    top: SCREEN_HEIGHT,
+    zIndex: 2,
+  },
+});
