5ccaf610d2a7db89da6d08177b05903b662b45db
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2023-07-10 12:56:09 -0300

    feat: create hook to bottom sheet

---
+++ src/hooks/useBottomSheet.ts
@@ -0,0 +1,21 @@
+import {useRef, useCallback} from 'react';
+
+import {BottomSheetRefProps} from '@components';
+
+export function useBottomSheet() {
+  const bottomSheetRef = useRef<BottomSheetRefProps>(null);
+
+  const handleToggleBottomSheet = useCallback(() => {
+    const isActive = bottomSheetRef?.current?.isActive();
+    if (isActive) {
+      bottomSheetRef?.current?.scrollTo(0);
+    } else {
+      bottomSheetRef?.current?.scrollTo(-200);
+    }
+  }, []);
+
+  return {
+    bottomSheetRef,
+    handleToggleBottomSheet,
+  };
+}
